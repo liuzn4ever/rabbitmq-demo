7bca611ec48639369e0792949c76b59a683fdd7e
liuzhinan <123456>
2020-06-23 15:09:34 +0800

    thrid commit

--- src/test/java/com/lzn/mq/rabbitmqdemo/RabbitmqDemoApplicationTests.java
+++ src/test/java/com/lzn/mq/rabbitmqdemo/RabbitmqDemoApplicationTests.java
@@ -1,13 +1,8 @@
 package com.lzn.mq.rabbitmqdemo;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RabbitmqDemoApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
